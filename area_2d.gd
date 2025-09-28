bad3d16dc39d240d79c17820a21ed168eb99ef8f
Braeden Sy Tan <braedenjairsytan@icloud.com>
2025-09-28 20:59:32 +1300

    Add files via upload

---
+++ area_2d.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# The next level to load when picked up
+var next_level_scene: String = "res://Level/SecondLevel.tscn"
+
+func _ready():
+	# Connect the signal to detect when the player touches the power-up
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(_body):
+	call_deferred("_change_scene")
+
+func _change_scene():
+	get_tree().change_scene_to_file("res://Level/SecondLevel.tscn")
